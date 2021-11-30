412b65aa8e19bf5891b578a6cbe926cc794680d4
MihailBeloshapkin <mihail.beloshapkin.mm@yandex.ru>
2021-11-30 14:38:21 +0300

    add table with deltas

--- l2/l2/Program.fs
+++ l2/l2/Program.fs
@@ -1,4 +1,6 @@
 open System
+open System.Collections
+open System.Collections.Generic
 
 let Split delta interval =
     [fst interval .. delta .. snd interval] |> List.fold (fun acc x -> (x, x + delta) :: acc) [] |> List.tail
@@ -31,4 +33,42 @@ let Lagrange x (data : list<float * float>) =
     let terms = [0 .. List.length data - 1] |> List.map (fun i  -> getTerm x i data)
     terms |> List.fold (fun acc x -> acc + x) 0.0
 
+
+let getNewDeltas (li : list<float>) (xValues : list<float>) iter = 
+    [for i in 1 .. li.Length - 1 do (li.[i] - li.[i - 1]) / (xValues.[i + iter] - xValues.[i - 1])]
+
+let getLists (table : list<list<float>>) =
+    let xValues = table |> List.rev |> List.head 
+    let rec sub (l : list<list<float>>) iter =
+        match l with
+        | [] -> failwith "List should be not empty"
+        | head::tail -> if head.Length = 1 then l else sub ((getNewDeltas head xValues iter) :: head :: tail) (iter + 1)
+    sub table 0
+
+// Newton interpolation method realization.
+let Newton x n (table : list<float * float>) =
+    // Sort table by x - x_n
+    let sortedTable = table |> List.sortBy (fun element -> Math.Abs((fst element) - x)) 
+    printfn "%A" sortedTable
+    let generalTable = [for i in 0 .. n - 1 do sortedTable.[i]]
+    printfn "%A" generalTable
+    let listOfDeltas = [generalTable |> List.map snd; generalTable |> List.map fst] |> getLists
+    printfn "%A" listOfDeltas
+
+
+
+
+
+let getXMultiplication (xValues : list<int>) x = 
+    let rec sub values acc =
+        match values with
+        | [] -> acc
+        | head :: tail -> sub tail (acc * (x - head))
+    sub xValues x
+
+// let ex = [1; 2; 4; 9]
+// let ex1 = [for i in 1 .. ex.Length - 1 do ex.[i] - ex.[i - 1]]
+Newton -1.2 4 [(-1.0, 9.0); (1.0, 3.0); (2.0, 3.0); (3.0, 5.0)]
+// printfn "%A" (getLists ex)
+printfn "Lab number 2" |> ignore
 printfn "%A" ((Lagrange 1.0 [(-1.0, 4.0); (0.0, 1.0); (2.0, 1.0)]) - 0.0) 
